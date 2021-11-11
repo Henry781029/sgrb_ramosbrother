c8731ed06a9518dce2187bd693bd2f8657bb5197
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-11-11 18:14:26 -0500

    Se crean vistas de productos

---
+++ app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/SGRB_User.java
@@ -0,0 +1,14 @@
+package com.project_moviles2.shopgamer_ramosbrother;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SGRB_User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sgrb_user);
+    }
+}
---
+++ app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/SGRB_ViewPurchasedProduct.java
@@ -0,0 +1,14 @@
+package com.project_moviles2.shopgamer_ramosbrother;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SGRB_ViewPurchasedProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sgrb_view_purchased_product);
+    }
+}
