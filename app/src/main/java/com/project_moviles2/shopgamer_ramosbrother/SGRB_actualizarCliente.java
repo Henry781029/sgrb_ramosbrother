912af5bea50c9906a916c6bcf6a7eb1d007ea23c
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-11-11 13:41:09 -0500

    se crean todas las vistas

---
+++ app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/SGRB_actualizarCliente.java
@@ -0,0 +1,14 @@
+package com.project_moviles2.shopgamer_ramosbrother;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SGRB_actualizarCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sgrb_actualizar_cliente);
+    }
+}
