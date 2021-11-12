8a12d0ae3b19896d07ce0a8e1cf8fe4fb693e389
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-11-12 01:45:46 -0500

    se inicia seseion

--- app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/MainActivity.java
+++ app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/MainActivity.java
@@ -1,14 +1,55 @@
 package com.project_moviles2.shopgamer_ramosbrother;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView jtvregistrarse;
+    EditText jetusuario, jetclave;
+    Button jbtloguear;
+
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        jtvregistrarse=findViewById(R.id.tvRegistrarse);
+        jetusuario=findViewById(R.id.etUsuario);
+        jetclave=findViewById(R.id.etClave);
+        jbtloguear=findViewById(R.id.btnloquear);
+
+        String email_retorno=getIntent().getStringExtra("coleccion");
+
+        if( email_retorno == null) {
+            jetusuario.setText("");
+        }
+        else{
+            jetusuario.setText(email_retorno);
+        }
+
     }
+
+    public void irRegistro (View view){
+
+        Intent intent= new Intent(getApplicationContext(),SGRB_register.class);
+        startActivity(intent);
+
+    }
+
+
+
 }
--- app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/SGRB_screenSplash.java
+++ app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/SGRB_screenSplash.java
@@ -2,13 +2,48 @@ package com.project_moviles2.shopgamer_ramosbrother;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SGRB_screenSplash extends AppCompatActivity {
+
+    TextView jtvtitulo, jtvslogan;
+    ImageView jimagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sgrb_screen_splash);
+
+        getSupportActionBar().hide();
+
+        jtvslogan=findViewById(R.id.tvSlogan);
+        jtvtitulo=findViewById(R.id.tvTitulo);
+        jimagen=findViewById(R.id.imagen);
+
+        Animation animation1= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.desplazamiento_arriba);
+        Animation animation2= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.desplazamiento_abajo);
+        Animation animation3= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.opaco);
+
+        jtvtitulo.setAnimation(animation1);
+        jtvslogan.setAnimation(animation2);
+        jimagen.setAnimation(animation3);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },4000);
+
+
     }
 }
