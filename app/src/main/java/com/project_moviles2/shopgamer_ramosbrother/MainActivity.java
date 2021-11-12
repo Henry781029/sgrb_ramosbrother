4330f827c052bd5f2d83fa87f37c6907b7a354fa
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-11-12 14:35:07 -0500

    se realiza conexion basde de datos

--- app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/MainActivity.java
+++ app/src/main/java/com/project_moviles2/shopgamer_ramosbrother/MainActivity.java
@@ -12,13 +12,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView jtvregistrarse;
     EditText jetusuario, jetclave;
     Button jbtloguear;
 
-   
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,85 @@ public class MainActivity extends AppCompatActivity {
             jetusuario.setText(email_retorno);
         }
 
+    }
+
+    public void logueo (View view) {
+
+        final String usuario, clave;
+
+        usuario = jetusuario.getText().toString();
+        clave = jetclave.getText().toString();
+
+        if (usuario.isEmpty()) {
+
+            Toast.makeText(getApplicationContext(), "Usuario no ingresado.", Toast.LENGTH_SHORT).show();
+            jetusuario.requestFocus();
+        } else if (clave.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Debe ingresar la contraseña.", Toast.LENGTH_SHORT).show();
+            jetclave.requestFocus();
+        }
+        else {
+
+            DocumentReference docRef = db.collection("users").document(usuario);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            Log.d("Mensaje1", "DocumentSnapshot data: " + document.getData());
+                            String password = document.getString("Password");
+
+                            if (password.equals(clave)) {
+                                String rol = document.getString("Rol");
+                                if (rol.equals("Vendedor")){
+
+                                    Intent intent = new Intent(getApplicationContext(), SGRB_Seller.class);
+                                    intent.putExtra("coleccion", usuario);
+                                    intent.putExtra("rol", rol);
+                                    intent.putExtra("password", password);
+                                    //guardarPreferencias();
+                                    startActivity(intent);
+
+                                }
+                                else if (rol.equals("User")){
+                                    Intent intent = new Intent(getApplicationContext(), SGRB_User.class);
+                                    intent.putExtra("coleccion", usuario);
+                                    intent.putExtra("rol", rol);
+                                    intent.putExtra("password", password);
+                                    startActivity(intent);
+
+                                }
+                                jetclave.setText("");
+
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                                jetclave.setText("");
+                                jetclave.requestFocus();
+                            }
+
+                        } else {
+                            Log.d("mensaje2", "No such document");
+                            Toast.makeText(getApplicationContext(), "Usuario no extiste, por favor confirmar", Toast.LENGTH_LONG).show();
+
+                            jetusuario.requestFocus();
+                            jetclave.setText("");
+                        }
+                    } else {
+
+                        Log.d("Mensaje3", "get failed with ", task.getException());
+                        // Toast.makeText(getApplicationContext(), "Usuario no extiste, por favor confirmar", Toast.LENGTH_LONG).show();
+
+                        jetusuario.requestFocus();
+                        jetclave.setText("");
+                    }
+
+                }
+
+            });
+
+        }
     }
 
     public void irRegistro (View view){
